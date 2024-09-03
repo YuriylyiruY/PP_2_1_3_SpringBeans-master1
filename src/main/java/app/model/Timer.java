2d03ab832bc13b1de44e8f297c2da64e1cb2351d
komen <ykomendantskiy@list.ru>
2024-09-03 21:34:32 +0400

    commit after test

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,14 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
